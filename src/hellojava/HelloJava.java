82cbc859ee5b5bc24e280b407f4d720129c67077
YuriiTrofimenko <tyaa@ukr.net>
2019-12-01 11:59:06 +0200

    init

---
+++ src/hellojava/HelloJava.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojava;
+
+/**
+ *
+ * @author student
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*System.out.println("Hello World!");
+        System.out.println("Hello Java!");
+        System.out.print("Hello ");
+        System.out.println("User!");
+        
+        // int x;
+        // int x = 0;
+        // Объявление переменной х целого типа
+        int x;
+        // Инициализация переменной х значением 10
+        x = 10;
+        // Присвоение переменной х значения 25
+        x = 25;
+        // System.out.println(0);
+        System.out.println(x);
+        System.out.println(x);
+        
+        // double d1 = 30;
+        // double d1 = 30.0;
+        double d1 = 30d;
+        
+        char ch1 = 'A';
+        ch1 = '2';
+        ch1 = '%';
+        System.out.println(ch1);
+        
+        boolean b1 = true;
+        b1 = false;
+        
+        // System.out.println(2d + 5d / 10d);
+        System.out.println((2d + 5d) / 10d);*/
+        
+        // Создать переменные целого типа a = 10, b = 120, c = -90,
+        // вывести в консоль результаты вычисления по формуле: a - b * c
+        
+        // Создать переменные дробного типа x1 = 10.05, y1 = 120.99, z1 = -90.1,
+        // вывести в консоль результаты вычисления по формуле: x1 / y1 * z1
+        
+        // lubayali1505@gmail.com
+    }
+    
+}
